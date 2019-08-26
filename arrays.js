f939b12d61c3f20a334319ef01feec5906c5a94e
D Y <yomtovdaniel1@gmail.com>
2019-08-26 08:37:38 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,12 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, para) {
-
+  var secondArray = array;
+  secondArray = [para, ...array];
 }
 
 function destructivelyAddElementToBeginningOfArray(array, para) {
-
+  var myArray = array;
+  array.push(para);
+  return myArray;
 }
